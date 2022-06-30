f645e684d20425a916800b6029b06d51f7d3e082
massimo cibeira <cibeiramassimo@gmail.com>
2022-06-29 22:01:48 -0300

    correo

---
+++ js/contacto.js
@@ -0,0 +1,12 @@
+// const $form = document.querySelector('#form');
+// const $buttonMailto = document.querySelector('#mailto')
+
+// $form.addEventListener('submit', handleSubmit);
+
+// function handleSubmit(event) {
+//     event.preventDefault()
+//     const form = new FormData(this)
+//     console.log(form.get('name'))
+//     $buttonMailto.setAttribute('href',`mailto:suport1machi1@gmail.com?subjet=${form.get('name')}${form.get('email')}&body=${form.get('message')}`)
+//     $buttonMailto.click()
+// }
